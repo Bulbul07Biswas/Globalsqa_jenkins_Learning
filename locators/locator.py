39b2cd64b4b1b7b0a20f64418da31d1573a25206
Bulbul Biswas <bulbul07biswas@gmail.com>
2026-05-12 11:48:37 +0530

    updated locator changes

--- locators/locator.py
+++ locators/locator.py
@@ -7,7 +7,7 @@ class Locators:
   search_page_header='SEARCH RESULTS FOR: SOFTWARE'
   search_page_option ="(//div[@class='post_item'])[4]"
   
-  #testinh github push
+  #Need to check new changes are reflecting or not
   
 #Home
 
