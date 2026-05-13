552d6f21277bcf1c3f452c4615a8d49abc9fb9fb
Bulbul Biswas <bulbul07biswas@gmail.com>
2026-05-13 13:27:36 +0530

    check Report_generation

--- locators/locator.py
+++ locators/locator.py
@@ -7,7 +7,7 @@ class Locators:
   search_page_header='SEARCH RESULTS FOR: SOFTWARE'
   search_page_option ="(//div[@class='post_item'])[4]"
   
-  #Check Jenkins for auto trigger_check
+  #Check Jenkins for auto trigger_check_result
   
 #Home
 
