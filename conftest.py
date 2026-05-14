f6335fe9dc6d8cf863d4e1c1b56ebd98e7eb8fa5
Bulbul Biswas <bulbul07biswas@gmail.com>
2026-05-14 15:30:49 +0530

    Check Docker changes

--- conftest.py
+++ conftest.py
@@ -6,10 +6,12 @@ from playwright.sync_api import sync_playwright
 
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, args=["--start-maximized"])
+        browser = p.chromium.launch(headless=True, args=["--start-maximized"])
         context = browser.new_context(no_viewport= True)
         page= context.new_page()
         page.goto("https://www.globalsqa.com/demo-site/#google_vignette")
         yield page
         context.close()
         browser.close()
+        
+    #Need to check docker 
